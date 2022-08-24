8e9acbf8a0343b267d18df35f95215911b97c4cf
Magdalena Szpitalna <madzikresist@gmail.com>
2022-08-24 15:47:29 +0200

    Footer and Home

--- src/components/About.js
+++ src/components/About.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import StarsComponent from 'components/Stars';
 import Nav from 'components/Nav';
 import myPhoto from 'images/myPhoto.jpg';
+import Footer from 'components/Footer';
 
 const Container = styled.div`
   width: 100vw;
@@ -101,27 +102,31 @@ const PhotoContainer = styled.div`
 `;
 const MyPhoto = styled.div`
   background: url(${myPhoto}) center/cover no-repeat;
+  background-size: contain;
   margin-top: 32px;
-  width: 150px;
-  height: 250px;
+  height: 290px;
+  width: 190px;
   position: relative;
-  border-radius: 10%;
   z-index: 15;
-
+  border: 1px solid white;
+  border-radius: 10px;
+  -webkit-box-shadow: -1px -4px 77px -1px rgba(103, 11, 133, 1);
+  -moz-box-shadow: -1px -4px 77px -1px rgba(103, 11, 133, 1);
+  box-shadow: -1px -4px 77px -1px rgba(103, 11, 133, 1);
   @media (min-width: 768px) {
     left: 0;
     transform: translateX(0);
     margin-top: 0;
     margin-left: 32px;
     width: 200px;
-    height: 300px;
+    height: 305px;
   }
   @media (min-width: 1000px) {
     left: 0;
     transform: translateX(0);
     margin-left: 32px;
-    width: 300px;
-    height: 400px;
+    width: 280px;
+    height: 430px;
   }
 `;
 const ExperienceContainer = styled.div`
@@ -177,12 +182,9 @@ const Icon = styled.svg`
   fill: white;
   width: 60px;
   height: 60px;
+  z-index: 15;
 `;
-const IconFooter = styled.svg`
-  fill: white;
-  width: 30px;
-  height: 30px;
-`;
+
 const TitleSkill = styled.h2`
   color: white;
   text-align: center;
@@ -230,19 +232,7 @@ const SkillBox = styled.div`
     padding: 20% 0;
   }
 `;
-const Footer = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-evenly;
-  width: 100vw;
-  height: auto;
-  background-color: #131019;
-  margin-top: 32px;
-  @media (min-width: 768px) {
-    margin-top: 10%;
-    padding-bottom: 8px;
-  }
-`;
+
 const About = () => {
   return (
     <>
@@ -257,8 +247,10 @@ const About = () => {
               </TitleKnowMe>
               <Text>
                 Hi! I am <span style={{ color: '#caa1d6' }}>Magdalena Szpitalna </span>from
-                <span style={{ color: '#caa1d6' }}> Poznań.</span> <br />I am 22 years old ICT
-                student at University of Technology in Poznań.
+                <span style={{ color: '#caa1d6' }}> Poznań.</span> <br />
+                <br />I am 22 years old ICT student at University of Technology in Poznań. <br />
+                <br />
+                In my spare time I play the piano and computer games.
               </Text>
             </TextContainer>
             <PhotoContainer>
@@ -357,7 +349,7 @@ const About = () => {
         <Title>
           <span style={{ color: '#caa1d6' }}>Tools </span> I use
         </Title>
-        <SkillSetContainer>
+        <SkillSetContainer style={{ marginBottom: '40px' }}>
           <SkillBox>
             <Icon xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32">
               <path d="M0 0v32h32v-32zM23.855 3.855c1.921 0 3.552 0.588 4.885 1.703l-1.48 2.224c-1.187-0.817-2.296-1.333-3.405-1.333-1.115 0-1.709 0.516-1.709 1.183v0.072c0 0.891 0.595 1.188 2.819 1.781 2.667 0.74 4.145 1.704 4.145 4v0.073c0 2.667-2 4.145-4.812 4.145-2-0.072-4-0.812-5.557-2.219l1.703-2.077c1.188 0.963 2.443 1.629 3.927 1.629 1.183 0 1.849-0.443 1.849-1.255v-0.079c0-0.739-0.443-1.109-2.667-1.703-2.667-0.667-4.292-1.407-4.292-4.073v-0.077c0-2.443 1.923-4 4.589-4zM2.369 4.145h3.037l2 7.709 2.296-7.709h2.224l2.224 7.709 2-7.709h3.109l-3.776 13.261h-2.52l-2.147-7.704-2.224 7.704h-2.443zM3.036 26h12v2h-12z" />
@@ -418,14 +410,7 @@ const About = () => {
             </Icon>
           </SkillBox>
         </SkillSetContainer>
-        <Footer>
-          <IconFooter xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512">
-            <path d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z" />
-          </IconFooter>
-          <IconFooter xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-            <path d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z" />
-          </IconFooter>
-        </Footer>
+        <Footer />
       </Container>
     </>
   );
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,13 @@
-import * as React from 'react';
+import React, { useRef } from 'react';
 import StarsComponent from 'components/Stars';
+import Footer from 'components/Footer';
 import styled from 'styled-components';
 import background2 from 'images/background2.png';
 import photo2 from 'images/photo2.png';
+import aboutPhoto from 'images/aboutPhoto.png';
 import Nav from 'components/Nav';
 import Typewriter from 'typewriter-effect';
+import emailjs from '@emailjs/browser';
 
 const Container = styled.div`
   width: 100vw;
@@ -96,7 +99,10 @@ const TypeWriterStyle = styled.h1`
   opacity: 40%;
   margin-top: 8vh;
   @media (min-width: 768px) {
-    font-size: 2.4rem;
+    font-size: 1.8rem;
+  }
+  @media (min-width: 1200px) {
+    font-size: 2rem;
   }
 `;
 const Photo = styled.div`
@@ -133,8 +139,11 @@ const Photo = styled.div`
 const Introduce = styled.div`
   width: 100%;
   position: relative;
-  height: 100vh;
-
+  height: auto;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  padding-top: 64px;
   background: rgb(23, 6, 38);
   background: linear-gradient(
     187deg,
@@ -143,8 +152,13 @@ const Introduce = styled.div`
     rgba(98, 34, 145, 1) 98%
   );
   @media (min-width: 768px) {
-    //background: rgb(43, 21, 70);
-    //background: linear-gradient(0deg, rgba(43, 21, 70, 1) 0%, rgba(25, 7, 41, 1) 100%);
+    width: 100%;
+    position: relative;
+    height: auto;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    padding-top: 64px;
   }
   @media (min-width: 1200px) {
   }
@@ -155,29 +169,164 @@ const About = styled.h1`
   color: #caa1d6;
   font-size: 2.5rem;
   position: relative;
-  z-index: 10;
+  z-index: 15;
   text-align: center;
 `;
-const IconFooter = styled.svg`
+
+const TextAbout = styled.p`
+  color: white;
+  font-size: 1.3rem;
+  width: 80%;
+  position: relative;
+  z-index: 15;
+  @media (min-width: 768px) {
+    width: 60%;
+    font-size: 1.5rem;
+  }
+`;
+const TextPhotoContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  width: 100%;
+  gap: 16px;
+  @media (min-width: 768px) {
+    flex-direction: row;
+    width: 80%;
+    margin-top: 32px;
+    gap: 0;
+  }
+  @media (min-width: 1200px) {
+    gap: 16px;
+    width: 65%;
+  }
+`;
+const Contact = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  margin-top: 48px;
+  margin-bottom: 72px;
+  position: relative;
+  z-index: 15;
+`;
+const PhotoAbout = styled.div`
+  background: url(${aboutPhoto}) center/cover;
+  width: 200px;
+  height: 200px;
+  @media (min-width: 400px) {
+    width: 300px;
+    height: 300px;
+  }
+  @media (min-width: 768px) {
+    width: 300px;
+    height: 300px;
+  }
+`;
+const Icons = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+  width: 100%;
+`;
+const StyledContactForm = styled.div`
+  width: 100vw;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  form {
+    width: 80%;
+    display: flex;
+    align-items: flex-start;
+    flex-direction: column;
+    font-size: 16px;
+
+    input {
+      width: 100%;
+      height: 35px;
+      padding: 7px;
+      outline: none;
+      border-radius: 5px;
+      border: 1px solid rgb(220, 220, 220);
+      -webkit-box-shadow: 0 1px 12px -2px rgba(135, 70, 144, 1);
+      -moz-box-shadow: 0 1px 12px -2px rgba(135, 70, 144, 1);
+      box-shadow: 0 1px 12px -2px rgba(135, 70, 144, 1);
+
+      &:focus {
+        border: 2px solid #707bd4;
+      }
+    }
+
+    textarea {
+      max-width: 100%;
+      min-width: 100%;
+      width: 100%;
+      max-height: 100px;
+      min-height: 100px;
+      padding: 7px;
+      outline: none;
+      border-radius: 5px;
+      border: 1px solid rgb(220, 220, 220);
+
+      &:focus {
+        border: 2px solid #707bd4;
+      }
+    }
+
+    label {
+      margin-top: 1rem;
+      color: white;
+      font-size: 1.3rem;
+    }
+
+    input[type='submit'] {
+      margin-top: 2rem;
+      cursor: pointer;
+      background: #e04c8e;
+      color: white;
+      border: none;
+      font-size: 1.5rem;
+      font-weight: 600;
+      padding-bottom: 32px;
+      transition: background ease-out 0.5s;
+
+      :hover {
+        background: #707bd4;
+      }
+    }
+  }
+
+  @media (min-width: 768px) {
+    width: 500px;
+    form {
+      width: 100%;
+    }
+  }
+`;
+const Icon = styled.svg`
   fill: white;
   width: 30px;
   height: 30px;
 `;
-const Footer = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-evenly;
-  width: 100vw;
-  height: auto;
-  background-color: #131019;
-  margin-top: 32px;
-  @media (min-width: 768px) {
-    margin-top: 10%;
-    padding-bottom: 8px;
-  }
-`;
-const TextAbout = styled.p``;
 const Home = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_97md4wc', 'template_bvivkch', form.current, 'J8sozXzUAlWcNHh6S').then(
+      (result) => {
+        console.log(result.text);
+        console.log('message sent');
+      },
+      (error) => {
+        console.log(error.text);
+      },
+    );
+  };
   return (
     <>
       <StarsComponent />
@@ -204,17 +353,58 @@ const Home = () => {
           <About>
             <span style={{ color: 'white' }}>About </span>me
           </About>
-          <TextAbout></TextAbout>
+          <TextPhotoContainer>
+            <TextAbout>
+              I am fluent in <span style={{ color: '#caa1d6' }}>JavaScript</span>, especially
+              <span style={{ color: '#caa1d6' }}> React.</span> <br /> <br />I am interested in
+              building websites and mobile applications. <br />
+              Whenever possible, I improve my programming skills and create new projects. <br />
+              <br />
+              Following technological trends allows me to update my knowledge.
+            </TextAbout>
+            <PhotoAbout />
+          </TextPhotoContainer>
+          <Contact>
+            <About>
+              Contact <span style={{ color: 'white' }}>with me </span>
+            </About>
+            <Icons>
+              <a
+                href="https://github.com/MadzikResist"
+                target="_blank"
+                rel="noreferrer"
+                style={{ zIndex: '15' }}
+              >
+                <Icon xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512">
+                  <path d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z" />
+                </Icon>
+              </a>
+              <a
+                href="https://www.linkedin.com/in/magdalena-szpitalna/"
+                target="_blank"
+                rel="noreferrer"
+                style={{ zIndex: '15' }}
+              >
+                <Icon xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                  <path d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z" />
+                </Icon>
+              </a>
+            </Icons>
+            <StyledContactForm>
+              <form ref={form} onSubmit={sendEmail}>
+                <label>Name</label>
+                <input type="text" name="user_name" />
+                <label>Email</label>
+                <input type="email" name="user_email" />
+                <label>Message</label>
+                <textarea name="message" />
+                <input type="submit" value="Send" />
+              </form>
+            </StyledContactForm>
+          </Contact>
         </Introduce>
-        <Footer>
-          <IconFooter xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512">
-            <path d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z" />
-          </IconFooter>
-          <IconFooter xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-            <path d="M416 32H31.9C14.3 32 0 46.5 0 64.3v383.4C0 465.5 14.3 480 31.9 480H416c17.6 0 32-14.5 32-32.3V64.3c0-17.8-14.4-32.3-32-32.3zM135.4 416H69V202.2h66.5V416zm-33.2-243c-21.3 0-38.5-17.3-38.5-38.5S80.9 96 102.2 96c21.2 0 38.5 17.3 38.5 38.5 0 21.3-17.2 38.5-38.5 38.5zm282.1 243h-66.4V312c0-24.8-.5-56.7-34.5-56.7-34.6 0-39.9 27-39.9 54.9V416h-66.4V202.2h63.7v29.2h.9c8.9-16.8 30.6-34.5 62.9-34.5 67.2 0 79.7 44.3 79.7 101.9V416z" />
-          </IconFooter>
-        </Footer>
       </Container>
+      <Footer />
     </>
   );
 };
